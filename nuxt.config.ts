45f8f59e6a03fe8a022ce9d4ec4c0f9b7bc234b4
Sathya Molagoda <masg.molagoda@gmail.com>
2024-04-16 13:38:01 +0400

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,7 @@ export default defineNuxtConfig({
       },
     },
   },
+  devServer: {
+    port: 3001
+  }
 });
--- pages/home.vue
+++ pages/home.vue
@@ -9,7 +9,7 @@
 -->
   <div class="min-h-full">
     <nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
@@ -283,14 +283,14 @@
     </nav>
 
     <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">
           Dashboard
         </h1>
       </div>
     </header>
     <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
       </div>
     </main>
